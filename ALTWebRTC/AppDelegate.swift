03bbb7758712c2b633665cda4bbaba77ed230607
Alfredo Luco <aluco@Alfredos-MacBook-Pro.local>
2020-06-06 17:15:55 -0400

    init

---
+++ ALTWebRTC/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  ALTWebRTC
+//
+//  Created by Alfredo Luco on 06-06-20.
+//  Copyright © 2020 Alfredo Luco. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+
+}
+
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '9.0'
+
+target 'ALTWebRTC' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for ALTWebRTC
+  pod 'Starscream'
+
+end
